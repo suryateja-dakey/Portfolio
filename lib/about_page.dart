7df1e3ff4dd25ee76ca2166e847080d772fc7051
suryatejadakey <suryateja.dakey@toucanus.com>
2024-06-12 12:38:02 +0530

    social media added

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -3,6 +3,9 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:rive/rive.dart' as rive;
 // import 'package:rive/rive.dart';
 import 'constants.dart';
+import 'package:social_media_buttons/social_media_button.dart';
+import 'package:social_media_buttons/social_media_buttons.dart';
+
 
 class AboutPage extends StatelessWidget {
   final Function(int) onTabSelected;
@@ -45,26 +48,26 @@ class AboutPage extends StatelessWidget {
                     ],
                   ),
                 ),
-                const SizedBox(height: 16),
+                const SizedBox(height: 10),
                 Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Row(
                     children: [
                       Flexible(
                         child: Text(
-                          'I\'m Surya, a passionate Flutter developer with a knack for crafting engaging and user-friendly mobile applications. I thrive in collaborative environments, working closely with designers and product managers to bring innovative ideas to life.\n\nMy current focus is on building beautiful and performant mobile apps using Flutter. I enjoy the challenge of translating complex functionalities into intuitive experiences that empower users.',
-                          style: GoogleFonts.caveat(
-                              color: Colors.white, fontSize: 30),
+                          'I\'m Surya, a passionate Flutter developer with a knack for crafting engaging and user-friendly mobile applications. I thrive in collaborative environments, working closely with designers and product managers to bring innovative ideas to life.\n\nMy current focus is on building beautiful and performant mobile apps as well as web apps using Flutter. I enjoy the challenge of translating complex functionalities into intuitive experiences that empower users.',
+                          style: GoogleFonts.kalam(
+                              color: Colors.white, fontSize: 20,fontWeight: FontWeight.w200),
                         ),
                       ),
                       const SizedBox(width: 16),
                       Stack(
                         children: [
-                           rive.RiveAnimation.asset(
-                            "assets/eclipse.riv",
-                            fit: BoxFit.cover,
-                            artboard: 'State Machine 1',
-                          ),
+                          //  rive.RiveAnimation.asset(
+                          //   "assets/eclipse.riv",
+                          //   fit: BoxFit.cover,
+                          //   artboard: 'State Machine 1',
+                          // ),
                           ElevatedButton.icon(
                             onPressed: () {
                               onTabSelected(1); // Navigate to works page
@@ -98,17 +101,20 @@ class AboutPage extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 10),
-                Row(
-                  children: [
-                    Flexible(
-                      child: Text(
-                        'Currently, I am helping retail investors to stay up-to-date with the financial market and grow their wealth at OKX.',
-                        style: GoogleFonts.caveat(
-                            color: Colors.white, fontSize: 30),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: [
+                      Flexible(
+                        child: Text(
+                          'Currently, I am helping retail investors to stay up-to-date with the financial market and grow their wealth at OKX.',
+                          style: GoogleFonts.kalam(
+                                color: Colors.white, fontSize: 20,fontWeight: FontWeight.w200),
+                        ),
                       ),
-                    ),
-                    Spacer(),
-                  ],
+                      Spacer(),
+                    ],
+                  ),
                 ),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
@@ -140,28 +146,29 @@ class AboutPage extends StatelessWidget {
                 const SizedBox(height: 8),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.facebook),
-                      color: Colors.white,
-                      onPressed: () {
-                        // Add your social media links
-                      },
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.telegram),
-                      color: Colors.white,
-                      onPressed: () {
-                        // Add your social media links
-                      },
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.g_mobiledata),
-                      color: Colors.white,
-                      onPressed: () {
-                        // Add your social media links
-                      },
-                    ),
+                  children: const [
+                      SocialMediaButton.linkedin(
+              url: "https://in.linkedin.com/in/suryateja-dakey-aa9852190?original_referer=https%3A%2F%2Fwww.google.com%2F",
+              size: 35,
+              color: Colors.blue,
+            ),
+                    SocialMediaButton.github(
+              url: "https://github.com/suryateja-dakey/Portfolio",
+              size: 35,
+              color: Colors.blue,
+            ),
+              SocialMediaButton.whatsapp(
+              url: "https://wa.me/918328166464",
+              size: 35,
+              color: Colors.green,
+            ),
+             SocialMediaButton.instagram(
+              url: "https://www.instagram.com/ifucook?igsh=a3E1OXU4MmxraDMx&utm_source=qr",
+              size: 35,
+              color: Colors.pink,
+            ),
+           
+           
                   ],
                 ),
                 buildConnectFooter(),
